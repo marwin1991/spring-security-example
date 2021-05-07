49a68be8b880f41d4c3e17e354175c5dd6682245
Peter <peter.zmilczak@gmail.com>
2021-05-07 20:32:49 +0200

    init project

---
+++ src/test/java/com/codecool/springsecurityexample/SpringSecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.springsecurityexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
